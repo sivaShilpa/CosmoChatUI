25569c284129710ec25563c298731b91e4b9ec21
sivaShilpa <shilpa.vchand@gmail.com>
2024-02-17 09:45:59 -0500

    change to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('data/db.json'); 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500; // <== You can change the port
 
